9478871f66da96abb59e7e45be324248f0a2a68e
Kauan <kanbme@protonmail.com>
2024-02-15 07:57:05 -0300

    Fixing app colors

--- app/src/main/java/br/studyleague/ui/components/Dialogs.kt
+++ app/src/main/java/br/studyleague/ui/components/Dialogs.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerColors
 import androidx.compose.material3.TimePickerDefaults
 import androidx.compose.material3.TimePickerLayoutType
 import androidx.compose.material3.rememberTimePickerState
@@ -41,9 +42,8 @@ fun TimePickerDialog(
         ) {
             TimePicker(
                 state = timePickerState,
-                colors = TimePickerDefaults.colors(
-                ),
                 layoutType = layoutType,
+                colors = TimePickerDefaults.colors(),
                 modifier = Modifier.padding(horizontal = 16.dp).padding(top = 10.dp),
             )
 
